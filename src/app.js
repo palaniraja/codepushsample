c359ffa2df158e15e4984f91a28e9097c35c06f4
palaniraja <palaniraja@live.com>
2017-04-01 19:15:16 +0530

    initial release

---
+++ src/app.js
@@ -0,0 +1,42 @@
+/**
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+export default class codepushSample extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Initial Version (v1.0)
+        </Text>
+        <Image source={require('./images/g-staff.png')} style={{width: 100, height: 163}} />
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
+
+AppRegistry.registerComponent('codepushSample', () => codepushSample);
