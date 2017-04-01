217f63ddb58be18d66716e8ed75ee4bd80b35a6b
palaniraja <palaniraja@live.com>
2017-04-01 19:18:19 +0530

    update release with code push

--- src/app.js
+++ src/app.js
@@ -11,14 +11,31 @@ import {
   Image,
 } from 'react-native';
 
+import codePush from "react-native-code-push";
+
 export default class codepushSample extends Component {
+
+
+componentDidMount(){
+    var updateDialogOptions = {
+          updateTitle: "Update",
+          optionalUpdateMessage: "New version of the app is available. Install?",
+          optionalIgnoreButtonLabel: "Later",
+          optionalInstallButtonLabel: "Yes",
+        };
+
+    codePush.sync({ updateDialog: updateDialogOptions});
+}
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Initial Version (v1.0)
+          Initial Version (v2.0)
         </Text>
         <Image source={require('./images/g-staff.png')} style={{width: 100, height: 163}} />
+        <Image source={require('./images/f-staff.png')} style={{width: 100, height: 163}} />
         
       </View>
     );
